9887897976da558645898e6c990791d7fd1141f6
Sergey Trifonov <trf@ya.ru>
2020-06-06 17:31:41 +0300

    The wheel initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "forome_tools",
+    version = "0.1",
+    py_modules = ['forome_tools'] ,
+    author = "Sergey Trifonov, with colleagues",
+    author_email = "trf@ya.ru",
+    description = "Miscellaneous tools used in Forome Platform",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/ForomePlatform/forome_misc_tool",
+    packages = setuptools.find_packages(),
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent"]
+)
